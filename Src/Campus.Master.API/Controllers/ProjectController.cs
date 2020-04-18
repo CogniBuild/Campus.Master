a29e0e82a4bb4e40b307131c5d2df9770b840b83
Anton Kozyriev <sonara4mus@gmail.com>
2020-04-18 20:20:03 +0300

    API: Add task related methods to the project controller

--- Src/Campus.Master.API/Controllers/ProjectController.cs
+++ Src/Campus.Master.API/Controllers/ProjectController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 using Campus.Master.API.Models.Project;
+using Campus.Master.API.Models.Task;
 using Campus.Master.API.Models;
 
 namespace Campus.Master.API.Controllers
@@ -122,14 +123,12 @@ namespace Campus.Master.API.Controllers
             
             // TODO: Put business logic here
             await Task.CompletedTask;
-            
-            var status = new StateTransfer
+
+            return Ok(new StateTransfer
             {
                 Message = "Project fields are updated now!",
                 Payload = ""
-            };
-            
-            return Ok(status);
+            });
         }
 
         /// <summary>
@@ -151,5 +150,70 @@ namespace Campus.Master.API.Controllers
                 Payload = ""
             });
         }
+        
+        /// <summary>
+        /// GET api/project/{id}/task?limit={number}&offset={number}
+        /// Authentication: Bearer {token}
+        /// Content-Type: application/json
+        /// </summary>
+        [HttpGet("{id}/task")]
+        public async Task<IActionResult> GetTasksRelatedToTheProject(int id, [FromQuery] int limit, [FromQuery] int offset)
+        {
+            _logger.LogInformation($"[{DateTime.Now} INFO] Get Tasks Related To The Project #{id}");
+            
+            // TODO: Put business logic here
+            
+            var result = await Task.Run(() => new [] 
+            {
+                new TaskModel {
+                    Id = 1,
+                    Description = "Description",
+                    Priority = "Priority",
+                    Tag = "Tag",
+                    Deadline = "00-00-0000"
+                },
+                new TaskModel {
+                    Id = 2,
+                    Description = "Description",
+                    Priority = "Priority",
+                    Tag = "Tag",
+                    Deadline = "00-00-0000"
+                }
+            });
+            
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// POST api/project/{id}/task
+        /// Authentication: Bearer {token}
+        /// Content-Type: application/json
+        ///
+        /// {
+        ///     “Description”: “...”,
+        ///     “Priority”: “...”,
+        ///     “Tag”: “...”,
+        ///     “Deadline”: “...”
+        /// }
+        /// </summary>
+        [HttpPost("{id}/task")]
+        public async Task<IActionResult> AttachTaskToTheProject(int id, [FromBody] TaskContentModel model)
+        {
+            _logger.LogInformation($"[{DateTime.Now} INFO] Attach Task To The Project #{id}");
+            
+            // TODO: Put business logic here
+            await Task.CompletedTask;
+            var newTaskId = 1;
+
+            var state = new StateTransfer
+            {
+                Message = $"Task '{model.Description}' is created!",
+                Payload = $"/project/{id}"
+            };
+
+            return Created(state.Payload, state);
+        }
+        
+        
     }
 }
