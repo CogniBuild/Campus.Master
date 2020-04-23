cf606c0962efc2cc552d445922d3ca98b08f10ff
Anton Kozyriev <sonara4mus@gmail.com>
2020-04-23 23:45:46 +0300

    Filter: correct section naming

--- Src/Campus.Master.API/Filters/QueryItemsLimiter.cs
+++ Src/Campus.Master.API/Filters/QueryItemsLimiter.cs
@@ -22,7 +22,7 @@ namespace Campus.Master.API.Filters
         
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            var limiterValue = Convert.ToInt32(_configuration["Endpoints:FetchedItemsCountLimit"]);
+            var limiterValue = Convert.ToInt32(_configuration["Endpoints:QueryItemsLimit"]);
             var requestedItemsValue = Convert.ToInt32(context.HttpContext.Request.Query[TargetQueryParameter]);
 
             if (requestedItemsValue > limiterValue)
