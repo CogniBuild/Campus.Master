abda91917db35fadbca5b97d8a30d849039b9e05
profileEnumerable <fustethernet@gmail.com>
2020-04-26 21:58:51 +0300

    folders/projects infrastructure for master service

---
+++ Src/Campus.Domain.Core/Models/AppUser.cs
@@ -0,0 +1,21 @@
+﻿namespace Campus.Domain.Core.Models
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+
+        public string Login { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string RegistrationDate { get; set; }
+
+        public int RoleId { get; set; }
+    }
+}
