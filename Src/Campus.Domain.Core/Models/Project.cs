79daa93f69d8a8104437665b729cf4b398e43605
profileEnumerable <fustethernet@gmail.com>
2020-04-26 21:58:51 +0300

    folders/projects infrastructure for master service

---
+++ Src/Campus.Domain.Core/Models/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace Campus.Domain.Core.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ Src/Campus.Domain.Interfaces/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace GamersParadise.Domain.Interfaces.Interfaces
+{
+    public class IUnitOfWork
+    {
+        
+    }
+}
