23f58f6d1f9868152473cacd78d9fd5ae00e76c0
Michael <fustethernet@gmail.com>
2020-05-03 20:53:05 +0300

    "should render title" test from app component deleted

--- UI/Campus-UI/src/app/app.component.spec.ts
+++ UI/Campus-UI/src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Campus-UI');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Campus-UI app is running!');
-  });
 });
