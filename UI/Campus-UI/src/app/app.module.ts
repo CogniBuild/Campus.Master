2fb74c279a7a65425faee84bf097edc8e8ea4591
profileEnumerable <45071639+profileEnumerable@users.noreply.github.com>
2020-05-11 21:51:51 +0300

    Sing in page (#12)
    * created sign-in component
    
    * add folder for styles(helpers)
    
    * add html&css tempalate sign-in page
    
    * add validation form and service for sign-in from
    
    * "should render title" test from app component deleted
    
    * Sing in page #12 - pr fixes
    
    Co-authored-by: yurii <fetkoyura@gmail.com>

--- UI/Campus-UI/src/app/app.module.ts
+++ UI/Campus-UI/src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SingInComponent} from './sing-in/sing-in.component';
+import {SignInService} from './shared/services/sign-in.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [SignInService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ UI/Campus-UI/src/app/sing-in/sing-in.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {User} from '../shared/interfaces';
+import {SignInService} from '../shared/services/sign-in.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-sing-in',
+  templateUrl: './sing-in.component.html',
+  styleUrls: ['./sing-in.component.sass']
+})
+export class SingInComponent implements OnInit {
+
+  form: FormGroup;
+
+  // constructor(private auth: SignInService, private router: Router) {
+  // }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  submit() {
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password
+    };
+
+    // this.auth.login(user).subscribe(() => {
+    //   this.form.reset();
+    //   this.router.navigate([]);
+    // });
+  }
+}
