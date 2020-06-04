9bdbebf3e27bf6696f9c41b8cd3c56647d1bde2e
profileEnumerable <fustethernet@gmail.com>
2020-06-04 19:09:45 +0300

    lint fix

--- UI/Campus-UI/src/app/views/task-list-layout/projects/projects.component.ts
+++ UI/Campus-UI/src/app/views/task-list-layout/projects/projects.component.ts
@@ -1,18 +1,17 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {DataHandlerService} from '../../../shared/services/data-handler.service';
-import {Category} from '../../../model/category';
-import {MatDialog} from '@angular/material/dialog';
-import {EditTaskDialogComponent} from "../../../dialog/edit-task-dialog/edit-task-dialog.component";
-import {OperType} from "../../../dialog/OperType";
-import {EditProjectDialogComponent} from "../../../dialog/edit-project-dialog/edit-project-dialog.component";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { DataHandlerService } from '../../../shared/services/data-handler.service';
+import { Category } from '../../../model/category';
+import { MatDialog } from '@angular/material/dialog';
+import { EditTaskDialogComponent } from '../../../dialog/edit-task-dialog/edit-task-dialog.component';
+import { OperType } from '../../../dialog/OperType';
+import { EditProjectDialogComponent } from '../../../dialog/edit-project-dialog/edit-project-dialog.component';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.sass']
+  styleUrls: ['./projects.component.sass'],
 })
 export class ProjectsComponent implements OnInit {
-
   @Input()
   categories: Category[];
 
@@ -30,12 +29,15 @@ export class ProjectsComponent implements OnInit {
 
   todayDate: Date = new Date();
 
-
-  constructor(private dataHandlerService: DataHandlerService, private dialog: MatDialog) {
-  }
+  constructor(
+    private dataHandlerService: DataHandlerService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
-    // this.dataHandlerService.getAllCategories().subscribe(categories => this.categories = categories);
+    this.dataHandlerService
+      .getAllCategories()
+      .subscribe((categories) => (this.categories = categories));
   }
 
   showTasksByCategory(category: Category) {
@@ -47,16 +49,15 @@ export class ProjectsComponent implements OnInit {
 
     console.log(this.selectedCategory);
     this.selectCategory.emit(this.selectedCategory);
-
   }
 
   openAddDialog() {
     const dialogRef = this.dialog.open(EditProjectDialogComponent, {
       data: ['', 'Add project', OperType.ADD],
-      width: '400px'
+      width: '400px',
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         this.addCategory.emit(result as string);
       }
--- UI/Campus-UI/src/app/views/task-list-layout/task-list-layout/task-list-layout.component.ts
+++ UI/Campus-UI/src/app/views/task-list-layout/task-list-layout/task-list-layout.component.ts
@@ -1,26 +1,26 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {Category} from '../../../model/category';
-import {DataHandlerService} from '../../../shared/services/data-handler.service';
-import {Task} from '../../../model/task';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Category } from '../../../model/category';
+import { DataHandlerService } from '../../../shared/services/data-handler.service';
+import { Task } from '../../../model/task';
 
 @Component({
   selector: 'app-task-list-layout',
   templateUrl: './task-list-layout.component.html',
   styleUrls: ['./task-list-layout.component.sass'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class TaskListLayoutComponent implements OnInit {
-
   categories: Category[];
   tasks: Task[];
   selectedCategory: Category = null;
   searchCategoryText = '';
 
-  constructor(private dataHandlerService: DataHandlerService) {
-  }
+  constructor(private dataHandlerService: DataHandlerService) {}
 
   ngOnInit(): void {
-    this.dataHandlerService.getAllCategories().subscribe(categories => this.categories = categories);
+    this.dataHandlerService
+      .getAllCategories()
+      .subscribe((categories) => (this.categories = categories));
 
     this.onSelectCategory(null);
   }
@@ -28,41 +28,30 @@ export class TaskListLayoutComponent implements OnInit {
   onSelectCategory(category: Category) {
     this.selectedCategory = category;
 
-    this.dataHandlerService.searchTasks(
-      this.selectedCategory,
-      null,
-      null,
-      null,
-    ).subscribe(tasks => {
-      this.tasks = tasks;
-    });// обирає таски для показу в таск ліст(показує відразу всі)
+    this.dataHandlerService
+      .searchTasks(this.selectedCategory, null, null, null)
+      .subscribe((tasks) => {
+        this.tasks = tasks;
+      }); // обирає таски для показу в таск ліст(показує відразу всі)
   }
 
   onUpdatedTask(task: Task) {
     this.dataHandlerService.updateTask(task).subscribe(() => {
-
-      this.dataHandlerService.searchTasks(
-        this.selectedCategory,
-        null,
-        null,
-        null
-      ).subscribe(tasks => {
-        this.tasks = tasks;
-      });
+      this.dataHandlerService
+        .searchTasks(this.selectedCategory, null, null, null)
+        .subscribe((tasks) => {
+          this.tasks = tasks;
+        });
     });
   }
 
   onDeleteTask(task: Task) {
-
     this.dataHandlerService.deleteTask(task.id).subscribe(() => {
-      this.dataHandlerService.searchTasks(
-        this.selectedCategory,
-        null,
-        null,
-        null
-      ).subscribe(tasks => {
-        this.tasks = tasks;
-      });
+      this.dataHandlerService
+        .searchTasks(this.selectedCategory, null, null, null)
+        .subscribe((tasks) => {
+          this.tasks = tasks;
+        });
     });
   }
 
@@ -73,18 +62,21 @@ export class TaskListLayoutComponent implements OnInit {
   }
 
   onAddCategory(title: string) {
-    this.dataHandlerService.addCategory(title).subscribe(() => this.updateCategories());
+    this.dataHandlerService
+      .addCategory(title)
+      .subscribe(() => this.updateCategories());
   }
 
   private updateCategories() {
-    this.dataHandlerService.getAllCategories().subscribe(categories => this.categories = categories);
+    this.dataHandlerService
+      .getAllCategories()
+      .subscribe((categories) => (this.categories = categories));
   }
 
   private onSearchCategory(title: string) {
-
     this.searchCategoryText = title;
 
-    this.dataHandlerService.searchCategories(title).subscribe(categories => {
+    this.dataHandlerService.searchCategories(title).subscribe((categories) => {
       this.categories = categories;
     });
   }
