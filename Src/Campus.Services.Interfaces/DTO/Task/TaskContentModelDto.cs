10715ece387a7d1894a4c9ede122157a5fc18eb6
profileEnumerable <fustethernet@gmail.com>
2020-06-06 17:51:23 +0300

    Extract getting user id to a separate method

---
+++ Src/Campus.Services.Interfaces/DTO/Task/TaskContentModelDto.cs
@@ -0,0 +1,10 @@
+namespace Campus.Services.Interfaces.DTO.Task
+{
+    public class TaskContentModelDto
+    {
+        public string Description { get; set; }
+        public string Priority { get; set; }
+        public string Tag { get; set; }
+        public string Deadline { get; set; }
+    }
+}
--- Src/Campus.Services.Interfaces/Interfaces/IProjectService.cs
+++ Src/Campus.Services.Interfaces/Interfaces/IProjectService.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Campus.Services.Interfaces.DTO.Project;
+using Campus.Services.Interfaces.DTO.Task;
 
 namespace Campus.Services.Interfaces.Interfaces
 {
@@ -9,5 +10,9 @@ namespace Campus.Services.Interfaces.Interfaces
         Task<IEnumerable<ProjectModelDto>> GetSavedProjects(int userId, int offset, int limit);
         Task<int> CreateProject(int userId, ProjectContentModelDto projectDto);
         Task<ProjectModelDto> GetProjectById(int userId, int projectId);
+        Task<int> EditProject(int userId,int id, ProjectContentModelDto projectContent);
+        Task<IEnumerable<TaskModelDto>> GetProjectTasks(int userId, int id, int limit, int offset);
+        Task<int> AddTaskToProject(int id, TaskContentModelDto model);
+        Task<int> DeleteProject(int userId, int projectId);
     }
 }
