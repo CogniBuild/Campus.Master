5a95f04ea910cf869d6daba586a79db6a37ddba1
Anton Kozyriev <sonara4mus@gmail.com>
2020-08-30 17:33:05 +0300

    Add test job to CI

--- UI/Campus-UI/jest.config.js
+++ UI/Campus-UI/jest.config.js
@@ -5,6 +5,6 @@ module.exports = {
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    collectCoverage: true,
+    collectCoverage: false,
     clearMocks: true
 };
