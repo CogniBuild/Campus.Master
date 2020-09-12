38cece0913bc55a64a88dc5dab64bf11793644a0
Anton Kozyriev <sonara4mus@gmail.com>
2020-09-12 15:31:05 +0300

    fix: remove login from Dto

--- Src/Campus.Services.Interfaces/DTO/Profile/ProfileViewDto.cs
+++ Src/Campus.Services.Interfaces/DTO/Profile/ProfileViewDto.cs
@@ -2,7 +2,6 @@ namespace Campus.Services.Interfaces.DTO.Profile
 {
     public class ProfileViewDto
     {
-        public string Login { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
