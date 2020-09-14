a6ac98a97b0e847cb42366e7318b0ff8a6f98c06
Anton Kozyriev <sonara4mus@gmail.com>
2020-09-14 22:09:55 +0300

    feat: setup base test service infrastructure

---
+++ Test/Campus.Master.IntegrationTests/BaseTest.cs
@@ -0,0 +1,41 @@
+using System;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using Campus.Domain.Interfaces.Interfaces;
+using Campus.Infrastructure.Data.EntityFrameworkCore.Context;
+using Campus.Infrastructure.Data.EntityFrameworkCore.Repositories;
+using Campus.Infrastructure.Business.Services;
+using Campus.Services.Interfaces.Interfaces;
+
+namespace Campus.Master.IntegrationTests
+{
+    public class BaseTest : IDisposable
+    {
+        public ServiceProvider Provider { get; }
+
+        public BaseTest()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddDbContext<CampusContext>(
+                options => options.UseInMemoryDatabase(databaseName: "CampusDb"));
+
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+            serviceCollection.AddScoped<IAppUserRepository, AppUserRepository>();
+            serviceCollection.AddScoped<IProjectRepository, ProjectRepository>();
+            serviceCollection.AddScoped<ITaskRepository, TaskRepository>();
+
+            serviceCollection.AddScoped<IAuthenticationService, AuthenticationService>();
+            serviceCollection.AddScoped<IProfileService, ProfileService>();
+            serviceCollection.AddScoped<IProjectService, ProjectService>();
+            serviceCollection.AddScoped<ITaskService, TaskService>();
+
+            Provider = serviceCollection.BuildServiceProvider();
+        }
+
+        public void Dispose() =>
+            Provider.Dispose();
+    }
+}
