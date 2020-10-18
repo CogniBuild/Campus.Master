9903c6c1688f4b8b823c928fd1c520442eccde63
Michael Datsenko <45071639+profileEnumerable@users.noreply.github.com>
2020-10-18 20:46:45 +0300

    remove HasDefaultValue for UserMaps (#49)

--- Src/Campus.Infrastructure.Data.EntityFrameworkCore/Configs/Mapping/UserMaps.cs
+++ Src/Campus.Infrastructure.Data.EntityFrameworkCore/Configs/Mapping/UserMaps.cs
@@ -33,7 +33,6 @@ namespace Campus.Infrastructure.Data.EntityFrameworkCore.Configs.Mapping
                 .IsRequired();
 
             builder.Property(u => u.Gender)
-                .HasDefaultValue(Gender.CantSay)
                 .HasConversion<string>();
 
             builder.HasOne(u => u.Role)
