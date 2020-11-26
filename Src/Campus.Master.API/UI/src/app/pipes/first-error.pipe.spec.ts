98bb72e139a79df24d3a992a5f449484113f1481
profileEnumerable <fustethernet@gmail.com>
2020-11-26 22:01:31 +0200

    tests for firstError pipe

--- Src/Campus.Master.API/UI/src/app/pipes/first-error.pipe.spec.ts
+++ Src/Campus.Master.API/UI/src/app/pipes/first-error.pipe.spec.ts
@@ -1,8 +1,20 @@
 import { FirstErrorPipe } from './first-error.pipe';
 
 describe('FirstErrorPipe', () => {
-  it('create an instance', () => {
+  test('create an instance', () => {
     const pipe = new FirstErrorPipe();
     expect(pipe).toBeTruthy();
+  });
+});
+
+describe('transform() should', () => {
+  const pipe = new FirstErrorPipe();
+
+  test('return first property name', () => {
+    expect(pipe.transform({ one: 1, two: 2 })).toMatch('one');
+  });
+
+  test('return null when value null', () => {
+    expect(pipe.transform(null)).toBeNull();
   });
 });
