f22d99702ed0a5eeeac6cfd2e97b3f67062ab2e5
profileEnumerable <fustethernet@gmail.com>
2020-12-27 14:49:57 +0200

    move redirect logic to guard

--- Src/Campus.Master.API/UI/src/app/core/guards/can-activate-dashboard.guard.ts
+++ Src/Campus.Master.API/UI/src/app/core/guards/can-activate-dashboard.guard.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { SignInService } from '../sign-in.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CanActivateDashboardGuard implements CanActivate {
 
-  constructor(private signInService: SignInService) {
+  constructor(private signInService: SignInService, private router: Router, private toastr: ToastrService) {
   }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> {
-    return this.signInService.isAuthenticated();
+    return this.signInService.getProfileInformation().pipe(
+      map(() => {
+        return true;
+      }),
+      catchError(() => {
+        this.router.navigate(['']);
+        this.toastr.error('Your are not authorized', '401');
+        return of(false);
+      })
+    );
   }
 }
--- Src/Campus.Master.API/UI/src/app/core/sign-in.service.ts
+++ Src/Campus.Master.API/UI/src/app/core/sign-in.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { AuthenticatedUser } from '../auth/shared/models/authenticated-user';
 import { HttpClient } from '@angular/common/http';
 import { StateTransfer } from '../models/state-transfer';
 import { ProfileInformation } from '../auth/shared/models/profile-information';
 import { environment } from 'environments/environment';
-import { catchError, map } from 'rxjs/operators';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class SignInService {
-  constructor(private http: HttpClient, private router: Router, private toastr: ToastrService) {
+  constructor(private http: HttpClient) {
   }
 
   login(user: AuthenticatedUser): Observable<StateTransfer> {
@@ -20,18 +17,5 @@ export class SignInService {
 
   getProfileInformation(): Observable<ProfileInformation> {
     return this.http.get<ProfileInformation>(environment.getProfileInformation);
-  }
-
-  isAuthenticated(): Observable<boolean> {
-    return this.getProfileInformation().pipe(
-      map(() => {
-        return true;
-      }),
-      catchError(() => {
-        this.router.navigate(['']);
-        this.toastr.error('Your are not authorized', '401');
-        return of(false);
-      })
-    );
   }
 }
