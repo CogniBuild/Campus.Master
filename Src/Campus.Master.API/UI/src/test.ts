e0ee635678017224d626bd49aa5a46a6618d46f8
profileEnumerable <fustethernet@gmail.com>
2021-01-07 12:04:05 +0200

    add test.ts file with configuration for JSDOM env

---
+++ Src/Campus.Master.API/UI/src/test.ts
@@ -0,0 +1,22 @@
+import 'jest-preset-angular';
+
+Object.defineProperty(window, 'CSS', { value: null });
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => {
+    return {
+      appearance: ['-webkit-appearance']
+    };
+  }
+});
+
+Object.defineProperty(document, 'doctype', {
+  value: '<!DOCTYPE html>'
+});
+Object.defineProperty(document.body.style, 'transform', {
+  value: () => {
+    return {
+      enumerable: true,
+      configurable: true
+    };
+  }
+});
