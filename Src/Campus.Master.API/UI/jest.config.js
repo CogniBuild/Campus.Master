99b0cc58ecf8b603e9bbd1fa44c9c9173541c19d
profileEnumerable <fustethernet@gmail.com>
2021-01-07 11:49:31 +0200

    add configs to fix error when run tests with TestBed

--- Src/Campus.Master.API/UI/jest.config.js
+++ Src/Campus.Master.API/UI/jest.config.js
@@ -1,10 +1,16 @@
+const {pathsToModuleNameMapper} = require("ts-jest/utils");
+const {compilerOptions} = require('./tsconfig');
+
 module.exports = {
-    roots: ['<rootDir>/src'],
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    collectCoverage: false,
-    clearMocks: true
+  preset: 'jest-preset-angular',
+  roots: ['<rootDir>/src'],
+  transform: {'^.+\\.tsx?$': 'ts-jest'},
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: false,
+  clearMocks: true,
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: '<rootDir>/'
+  })
 };
