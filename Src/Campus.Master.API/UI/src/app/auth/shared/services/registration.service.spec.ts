3a81817ee2384be5761c5eaa53f60abbe0ae73ff
Michael Datsenko <45071639+profileEnumerable@users.noreply.github.com>
2021-01-13 20:15:23 +0200

    Unit tests registration service (#60)
    * add configs to fix error when run tests with TestBed
    
    * add test.ts file with configuration for JSDOM env
    
    * replace testRegex with testMatch to avoid including test.ts file to test run
    
    * update tsconfig.spec.json
    
    * remove test section from angular.json
    
    * test for returned state transfer in registration service
    
    * add should failed with 400 test

---
+++ Src/Campus.Master.API/UI/src/app/auth/shared/services/registration.service.spec.ts
@@ -0,0 +1,70 @@
+import { RegistrationService } from './registration.service';
+import { RegisterUser } from '../models/register-user';
+import { StateTransfer } from '@shared-models/state-transfer';
+
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { HttpErrorResponse } from '@angular/common/http';
+
+describe('registerUser()', () => {
+  let httpTestingController: HttpTestingController;
+  let registrationService: RegistrationService;
+
+  const mockUser: RegisterUser = {
+    password: 'Campus321',
+    confirmPassword: 'Campus321',
+    email: 'example@example.com',
+    firstName: 'firstName',
+    lastName: 'lastName',
+    gender: 1
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [RegistrationService],
+      imports: [HttpClientTestingModule]
+    });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+    registrationService = TestBed.inject(RegistrationService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  test('should return state transfer when user created', () => {
+    const mockStateTransfer: StateTransfer = { message: 'state', payload: 'api/profile' };
+
+    registrationService.registerUser(mockUser)
+      .subscribe(stateTransfer => {
+          expect(stateTransfer).toEqual(mockStateTransfer);
+        }
+      );
+
+    const req = httpTestingController.expectOne('/api/profile/create');
+    expect(req.request.method).toEqual('POST');
+
+    req.flush(mockStateTransfer);
+  });
+
+  test('should failed with 400 error when user exists', (done) => {
+    const errorMsg = 'User already exists';
+
+    registrationService.registerUser(mockUser)
+      .subscribe((_) => {
+          fail('should failed with the 400 error');
+          done();
+        },
+        (error: HttpErrorResponse) => {
+          expect(error.status).toBe(400);
+          expect(error.error).toMatch(errorMsg);
+          done();
+        }
+      );
+
+    const req = httpTestingController.expectOne('/api/profile/create');
+
+    req.flush(errorMsg, { status: 400, statusText: 'Bad Request' });
+  });
+});
