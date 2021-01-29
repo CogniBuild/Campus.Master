1c668c81338e9f3f3d40e70cdea30d32a42cce66
profileEnumerable <fustethernet@gmail.com>
2021-01-29 16:57:26 +0200

    add empty calendar module

--- Src/Campus.Master.API/UI/src/app/app-routing.module.ts
+++ Src/Campus.Master.API/UI/src/app/app-routing.module.ts
@@ -8,7 +8,11 @@ const routes: Routes = [
   { path: '', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
   {
     path: 'campus', component: UserPageLayoutComponent, canActivate: [CanActivateDashboardGuard]
-  }
+    , children: [
+      {
+        path: 'calendar', loadChildren: () => import('./modules/calendar/calendar.module').then(m => m.CalendarModule)
+      }]
+  },
 ];
 
 @NgModule({
