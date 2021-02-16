90c10267d5152c9a3a9cb77fb089e461670655ac
profileEnumerable <fustethernet@gmail.com>
2021-02-16 14:05:10 +0200

    fix calendar.component test module configuration

--- Src/Campus.Master.API/UI/src/app/modules/calendar/calendar.component.spec.ts
+++ Src/Campus.Master.API/UI/src/app/modules/calendar/calendar.component.spec.ts
@@ -1,16 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalendarComponent } from './calendar.component';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { MatCardModule } from '@angular/material/card';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 describe('CalendarComponent', () => {
   let component: CalendarComponent;
   let fixture: ComponentFixture<CalendarComponent>;
 
   beforeEach(async(() => {
+    FullCalendarModule.registerPlugins([
+      dayGridPlugin,
+      timeGridPlugin,
+      interactionPlugin
+    ]);
+
     TestBed.configureTestingModule({
-      declarations: [ CalendarComponent ]
+      imports: [FullCalendarModule, MatCardModule],
+      declarations: [CalendarComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
