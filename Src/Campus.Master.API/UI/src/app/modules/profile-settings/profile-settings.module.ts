72a39a4d4b6f98fd623653e3f9c74596b2e621ad
profileEnumerable <fustethernet@gmail.com>
2021-06-03 22:44:42 +0300

    feat(profile-settings): basic layout for settings cards

--- Src/Campus.Master.API/UI/src/app/modules/profile-settings/profile-settings.module.ts
+++ Src/Campus.Master.API/UI/src/app/modules/profile-settings/profile-settings.module.ts
@@ -5,12 +5,13 @@ import { ProfileSettingsRoutingModule } from './profile-settings-routing.module'
 import { ProfileSettingsComponent } from './profile-settings.component';
 import { GeneralSettingsComponent } from './settings-groups/general-settings/general-settings.component';
 import { EditProfileComponent } from './settings-groups/edit-profile/edit-profile.component';
+import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
   declarations: [ProfileSettingsComponent, GeneralSettingsComponent, EditProfileComponent],
   imports: [
-    CommonModule,
+    SharedModule,
     ProfileSettingsRoutingModule
   ]
 })
