4849da30e7021897960ac44c3e370e2bedb60857
Michael Datsenko <45071639+profileEnumerable@users.noreply.github.com>
2021-06-13 14:49:37 +0300

    User settings page basic layout (#78)
    * feat(profile-settings): add empty module
    
    * feat(profile-settings): basic routing for settings groups
    
    * feat(profile-settings): basic layout for settings cards
    
    * feat(profile-settings): add default loading of general settings
    
    * feat(profile-settings): adaptive height for mateterial components
    
    * feat(profile-settings): align settings card items
    
    * feat(profile-settings): settings menu items layout
    
    * fix(ui-tests): remove default spec files for profile-module

---
+++ Src/Campus.Master.API/UI/src/app/modules/profile-settings/profile-settings-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ProfileSettingsComponent } from './profile-settings.component';
+import { GeneralSettingsComponent } from './settings-groups/general-settings/general-settings.component';
+import { EditProfileComponent } from './settings-groups/edit-profile/edit-profile.component';
+
+const routes: Routes = [{
+  path: '', component: ProfileSettingsComponent, children: [
+    {
+      path: '', redirectTo: 'general', pathMatch: 'full'
+    },
+    {
+      path: 'general', component: GeneralSettingsComponent,
+    },
+    {
+      path: 'edit-profile', component: EditProfileComponent
+    }
+  ]
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProfileSettingsRoutingModule {
+}
